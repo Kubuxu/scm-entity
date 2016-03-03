78f2f5717c3eb895b0d18d83ffd3d4fcfafa99d1
Jakub Sztandera <kubuxu@protonmail.ch>
2016-03-03 17:40:25 +0100

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+### scm-entity
+Entity is scheme based IRC bot.
+
+#### Using
+You have to install chicken and following modules: `sandbox irc symbol-utils`.
+
+### License
+
+Copyright © 2016 Jakub Sztandera \<k.sztandera@protonmail.ch\>
+
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To But It's Not My Fault, Version1
+as published by Ben McGinnes. See the LICENSE file for more details.
+
+
---
+++ start.scm
@@ -0,0 +1,65 @@
+#!/usr/bin/csi -s
+(require-extension sandbox irc posix symbol-utils srfi-1 srfi-13)
+
+(define-syntax try
+  (syntax-rules (catch)
+                ((_ body (catch catcher))
+                 (call-with-current-continuation
+                   (lambda (exit)
+                     (with-exception-handler
+                       (lambda (condition)
+                         catcher
+                         (exit condition))
+                       (lambda () body)))))))
+(define con
+  (irc:connection server: "localhost" nick: "Entity") )
+
+(define (irc:message-resp msg)
+  (define target (car (irc:message-parameters msg)))
+  (if (char=? #\# (string-ref target 0))
+    target
+    (car (irc:message-prefix msg)) ) )
+(define (irc:message-msg msg)
+  (cadr (irc:message-parameters msg)) )
+
+(define (string-truncate s len)
+  (string-take s (min (string-length s) len)) )
+
+(define exec-env 
+  (make-safe-environment parent: default-safe-environment mutable: #t extendable: #t) )
+(define (exec-safe str)
+  (define res
+    (try (safe-eval (call-with-input-string str read) environment: exec-env)
+         (catch #f)) ) 
+  (if (condition? res)
+    (get-condition-property res 'exn 'message "frak: no message supplied")
+    res) ) 
+
+(define (repl msg)
+  (define resp (exec-safe (string-drop (irc:message-msg msg) 1)))
+  (if (not (unspecified? resp))
+    (irc:say
+      con
+      (string-truncate
+        (format "~a: ~s" (car (irc:message-prefix msg))  resp)
+        512)
+      (irc:message-resp msg)) ) )
+
+(irc:connect con)
+
+(for-each (lambda (name) (safe-environment-set! exec-env name (eval name)))
+          (list 'fold 'fold-right 'reduce) )
+
+(irc:join con "#V")
+(irc:join con "#cjdns")
+
+(irc:add-message-handler!
+  con repl
+  command: "PRIVMSG"
+  body: (lambda (msg)
+          (define body (irc:message-msg msg))
+          (and (string? body) (string=? "?" (string-take body 1)))
+          ) )
+
+(irc:run-message-loop con debug: #t)
+;; vim: set expandtab ts=2 sw=2:
